1a27d1553fc8172c9be35e9cc3676a6c2405044d
David Edmondson <dme@dme.org>
2010-11-25 14:08:21 +0000

    test: Add some more emacs tests.
    Simple version of the `notmuch-hello' test, some tests of the matrix
    reflection code and the 're:' stripping code.

--- emacs/notmuch-test.el
+++ emacs/notmuch-test.el
@@ -23,6 +23,23 @@
 
 ;;
 
+(defun notmuch-test-file-as-string (filename)
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (buffer-string)))
+
+;; This tends not to work well except in batch mode.
+(defun notmuch-test-expected-result (name)
+  "Get the expected result of a test as a string."
+  (notmuch-test-file-as-string
+   (concat (getenv "EXPECTED") "/" name)))
+
+(defun notmuch-test-buffer-result (fn)
+  (funcall fn)
+  (buffer-substring-no-properties (point-min) (point-max)))
+
+;;
+
 (require 'notmuch-maildir-fcc)
 
 (ert-deftest notmuch-fcc-trivial ()
@@ -85,12 +102,49 @@
 
 ;;
 
-(defun notmuch-test ()
-  "Run the notmuch ERT tests."
-  (interactive)
+(require 'notmuch-hello)
 
-  (ert t))
+(ert-deftest notmuch-hello ()
+  "Check that `notmuch-hello' outputs correct data.
+
+Presumes that the email corpus is already present."
+
+  (should (string= (notmuch-test-buffer-result 'notmuch-hello)
+		   (notmuch-test-expected-result "notmuch-hello"))))
+
+(ert-deftest notmuch-hello-reflect ()
+  (should (equal '(1 4 7 10 2 5 8 11 3 6 9 12)
+		 (notmuch-hello-reflect '(1 2 3 4 5 6 7 8 9 10 11 12) 4)))
+
+  (should (equal '(1 4 7 10 2 5 8 nil 3 6 9 nil)
+		 (notmuch-hello-reflect '(1 2 3 4 5 6 7 8 9 10) 4))))
+
+;;
+
+(require 'notmuch-show)
+
+(ert-deftest notmuch-show-strip-re ()
+  (mapc '(lambda (test)
+	   (should (string= "fish"
+			    (notmuch-show-strip-re test))))
+	'("fish" "re: fish" "Re: fish" "RE: fish"))
+
+  (mapc '(lambda (test)
+	   (should (string= "some fish"
+			    (notmuch-show-strip-re test))))
+	'("some fish" "re: some fish" "some re: fish"))
+
+  (mapc '(lambda (test)
+	   (should-not (string= "some fish"
+				(notmuch-show-strip-re test))))
+	'(" some fish" "re: some fish " "somere: fish" "some fish re:")))
+
+;; 
 
 (defun notmuch-test-batch ()
   "Run the notmuch ERT tests in batch mode."
+
+  ;; Avoid the 10 column default of `emacs --batch'.
+  (set-frame-width (window-frame (get-buffer-window)) 80)
+
   (ert-run-tests-batch-and-exit))
